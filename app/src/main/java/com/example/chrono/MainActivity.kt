b0025fc7cf9e3356229d5d560fd7f0f89d2aae99
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-04-27 13:20:57 -0400

    enabled data binding

--- app/src/main/java/com/example/chrono/MainActivity.kt
+++ app/src/main/java/com/example/chrono/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.chrono
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.chrono.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var bind: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
