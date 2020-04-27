00b0c85207e6ed2586a806d634b0def14c939304
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-04-27 18:09:05 -0400

    ghetto toggle added

--- app/src/main/java/com/example/chrono/MainActivity.kt
+++ app/src/main/java/com/example/chrono/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.chrono
 
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.chrono.databinding.ActivityMainBinding
 import com.example.chrono.util.BaseActivity
@@ -11,5 +12,15 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        bind!!.navtoggle.setOnClickListener {
+            if (bind!!.navtoggle.isChecked) {
+                bind!!.swatchlay.visibility = View.VISIBLE
+                bind!!.timerlay.visibility = View.GONE
+            } else {
+                bind!!.swatchlay.visibility = View.GONE
+                bind!!.timerlay.visibility = View.VISIBLE
+            }
+        }
     }
 }
