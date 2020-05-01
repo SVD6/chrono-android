87835e7b444354a1126f4d0ae80a78d89af18cee
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-04-30 23:46:32 -0400

    tab layout complete with animations and light/dark theme

--- app/src/main/java/com/example/chrono/main/MainActivity.kt
+++ app/src/main/java/com/example/chrono/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.chrono.main
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -12,10 +13,10 @@ import androidx.viewpager.widget.ViewPager
 import com.example.chrono.R
 import com.example.chrono.databinding.ActivityMainBinding
 import com.example.chrono.util.BaseActivity
-import com.example.chrono.util.helpers.NonSwipeableViewPager
 import com.example.chrono.util.helpers.TabFragmentAdapter
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var timerFrag: TimerFrag
@@ -24,7 +25,9 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     var pager: ViewPager? = null
     var bind: ActivityMainBinding? = null
     var indicator: View? = null
-    var tablay: TabLayout? = null
+    private var tablay: TabLayout? = null
+
+    private var indicatorWidth: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +43,9 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         tablay!!.setupWithViewPager(pager)
 
         if (isUsingNightModeResources()) {
-            tablay!!.background = ContextCompat.getDrawable(this, R.drawable.tab_dark)
+            tablay!!.background = ContextCompat.getDrawable(this, R.drawable.nav_tab_dark)
         } else {
-            tablay!!.background = ContextCompat.getDrawable(this, R.drawable.tab_light)
+            tablay!!.background = ContextCompat.getDrawable(this, R.drawable.nav_tab_light)
         }
 
     }
