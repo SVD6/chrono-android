0ac23f496d2d8e26e20257aefd5dc5142f7450af
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-05-02 17:25:12 -0400

    cleaned up some code

--- app/src/main/java/com/example/chrono/main/MainActivity.kt
+++ app/src/main/java/com/example/chrono/main/MainActivity.kt
@@ -13,28 +13,21 @@ import androidx.viewpager.widget.ViewPager
 import com.example.chrono.R
 import com.example.chrono.databinding.ActivityMainBinding
 import com.example.chrono.util.BaseActivity
-import com.example.chrono.util.helpers.TabFragmentAdapter
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var timerFrag: TimerFrag
-    private lateinit var swatchFrag: StopwatchFrag
 
     var pager: ViewPager? = null
     var bind: ActivityMainBinding? = null
-    var indicator: View? = null
     private var tablay: TabLayout? = null
-
-    private var indicatorWidth: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         pager = bind!!.pager
-        indicator = bind!!.indicator
         tablay = bind!!.tablayout
 
         val fragmentAdapter = TabFragmentAdapter(supportFragmentManager)
@@ -47,10 +40,40 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         } else {
             tablay!!.background = ContextCompat.getDrawable(this, R.drawable.nav_tab_light)
         }
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         TODO("Not yet implemented")
     }
+
+    private inner class TabFragmentAdapter(fm: FragmentManager) :
+        FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> {
+                    TimerFrag()
+                }
+                else -> {
+                    StopwatchFrag()
+                }
+            }
+        }
+
+        override fun getCount(): Int {
+            return 2
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return when (position) {
+                0 -> "Timer"
+                else -> {
+                    return "Stopwatch"
+                }
+            }
+        }
+
+    }
 }
+
+
