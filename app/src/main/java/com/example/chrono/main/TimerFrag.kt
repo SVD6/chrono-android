cb6dec94409d85d6affa4d6aadad54960155b449
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-05-04 13:01:53 -0400

    timer working with bugs

--- app/src/main/java/com/example/chrono/main/TimerFrag.kt
+++ app/src/main/java/com/example/chrono/main/TimerFrag.kt
@@ -5,11 +5,14 @@ import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.chrono.R
 import com.example.chrono.databinding.FragmentTimerBinding
+import com.example.chrono.util.PrefUtil
 
 class TimerFrag : Fragment() {
     var bind: FragmentTimerBinding? = null
@@ -22,6 +25,11 @@ class TimerFrag : Fragment() {
     private var timerLengthSeconds: Long = 0
     private var timerState = TimerState.Stopped
 
+    private var secondsRemaining: Long = 0
+
+    private var progressCountdown: ProgressBar? = null
+    private var countdown: TextView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,18 +37,127 @@ class TimerFrag : Fragment() {
     ): View? {
         bind = DataBindingUtil.inflate(inflater, R.layout.fragment_timer, container, false)
 
+        progressCountdown = bind!!.progressbar
+        countdown = bind!!.countdown
+
         // Change up some UI elements based on light/dark mode
         if ((activity as MainActivity).isUsingNightModeResources()) {
             bind!!.progressbar.background =
-                ContextCompat.getDrawable(context!!, R.drawable.circle_dark)
+                ContextCompat.getDrawable(requireContext(), R.drawable.circle_dark)
         } else {
             bind!!.progressbar.background =
-                ContextCompat.getDrawable(context!!, R.drawable.circle_light)
+                ContextCompat.getDrawable(requireContext(), R.drawable.circle_light)
         }
 
+        bind!!.timerlayout.setOnClickListener {
+            when (timerState) {
+                TimerState.Paused -> {
+                    startTimer()
+                    timerState = TimerState.Running
+                }
+                TimerState.Running -> {
+                    timer.cancel()
+                    timerState = TimerState.Paused
+                }
+            }
+        }
 
+        bind!!.startstopbutton.setOnClickListener {
+            when (timerState) {
+                TimerState.Paused -> {
+                    startTimer()
+                }
+            }
+            startTimer()
+            timerState = TimerState.Running
+        }
 
         return bind!!.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        initTimer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (timerState == TimerState.Running) {
+            timer.cancel()
+        } else if (timerState == TimerState.Paused) {
+
+        }
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, requireContext())
+        PrefUtil.setSecondsRemaining(secondsRemaining, requireContext())
+        PrefUtil.setTimerState(timerState, requireContext())
+    }
+
+    private fun initTimer() {
+        timerState = PrefUtil.getTimerState(requireContext())
+
+        if (timerState == TimerState.Stopped)
+            setNewTimerLength()
+        else
+            setPreviousTimerLength()
+
+        secondsRemaining = if (timerState == TimerState.Running || timerState == TimerState.Paused)
+            PrefUtil.getSecondsRemaining(requireContext())
+        else
+            timerLengthSeconds
+
+        if (timerState == TimerState.Running)
+            startTimer()
+
+        updateCountdownUI()
+    }
+
+    private fun onTimerFinished() {
+        timerState = TimerState.Stopped
+
+        setNewTimerLength()
+
+        progressCountdown!!.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, requireContext())
+        secondsRemaining = timerLengthSeconds
+
+        updateCountdownUI()
+    }
+
+    private fun startTimer() {
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+                updateCountdownUI()
+            }
+        }.start()
+    }
+
+    private fun setNewTimerLength() {
+        val lengthInMinutes = PrefUtil.getTimerLength(requireContext())
+        timerLengthSeconds = (lengthInMinutes * 60L)
+        progressCountdown!!.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength() {
+        timerLengthSeconds = PrefUtil.getPreviousTimerLengthSeconds(requireContext())
+        progressCountdown!!.max = timerLengthSeconds.toInt()
+    }
+
+    private fun updateCountdownUI() {
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+
+        countdown!!.text =
+            "$minutesUntilFinished:${if (secondsStr.length == 2) secondsStr else "0" + secondsStr}"
+        progressCountdown!!.progress = (timerLengthSeconds - secondsRemaining).toInt()
     }
 
 }
