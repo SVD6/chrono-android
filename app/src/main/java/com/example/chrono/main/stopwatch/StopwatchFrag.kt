04bc464318caf99e4c8e48d61a2523431d686254
sahil sharma <sahilsharma@cmail.carleton.ca>
2020-06-20 16:32:21 -0400

    BROKEN: trying to add lap functionality

--- app/src/main/java/com/example/chrono/main/stopwatch/StopwatchFrag.kt
+++ app/src/main/java/com/example/chrono/main/stopwatch/StopwatchFrag.kt
@@ -1,10 +1,12 @@
 package com.example.chrono.main.stopwatch
 
+import android.content.Context
 import android.os.Bundle
 import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -12,13 +14,18 @@ import com.example.chrono.R
 import com.example.chrono.databinding.FragmentStopwatchBinding
 import com.example.chrono.util.components.MyChronometer
 import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.fragment_stopwatch.*
+import kotlinx.android.synthetic.main.reset_lap_button.*
+
 
 class StopwatchFrag : Fragment() {
     var bind: FragmentStopwatchBinding? = null
     var isPlaying: Boolean = false
+    var init_stopwatch = 0 // Clock has not been initialized
 
     var chronometer: MyChronometer? = null
     var startstopbutton: MaterialButton? = null
+    var resetlapbutton: MaterialButton? = null
     var offset: Int = 0
 
     override fun onCreateView(
@@ -41,7 +48,7 @@ class StopwatchFrag : Fragment() {
             }
         }
 
-        bind!!.resetbutton.setOnClickListener { reset() }
+//        bind!!.resetbutton.setOnClickListener { reset() }
 
         chronometer!!.setOnClickListener {
             if (!isPlaying) {
@@ -61,6 +68,13 @@ class StopwatchFrag : Fragment() {
     }
 
     private fun playingUI() {
+        if (init_stopwatch == 0) { //The stopwatch has not been initialized and this is the first instance of it starting.
+            // Then we add a reset/lap button
+            val resetLapBtn = resetlapbutton
+            buttons.addView(resetLapBtn)
+            init_stopwatch = 1 //Init is done
+        }
+
         chronometer!!.base = SystemClock.elapsedRealtime() - offset
         chronometer!!.start()
         isPlaying = true
