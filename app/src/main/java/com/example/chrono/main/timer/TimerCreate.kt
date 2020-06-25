ed612c6f275b8c27f5843b23d64ec1f68109309e
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-06-24 21:23:53 -0400

    ADR-8: Almost complete button layout
    updated gradle for carousel, changed the green colour

--- app/src/main/java/com/example/chrono/main/timer/TimerCreate.kt
+++ app/src/main/java/com/example/chrono/main/timer/TimerCreate.kt
@@ -2,12 +2,32 @@ package com.example.chrono.main.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.chrono.R
+import com.example.chrono.databinding.ActivityTimerCreateBinding
 
 class TimerCreate : AppCompatActivity() {
 
+    var bind: ActivityTimerCreateBinding? = null
+//    var carousel: CarouselPicker? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer_create)
+
+        bind = DataBindingUtil.setContentView(this, R.layout.create_timer_lay)
+//        carousel = bind!!.carousel
+//
+//        var icon_list: ArrayList<CarouselPicker.PickerItem> = ArrayList()
+//        icon_list.add(CarouselPicker.DrawableItem(R.drawable.ic_abs))
+//        icon_list.add(CarouselPicker.DrawableItem(R.drawable.ic_abs))
+//        icon_list.add(CarouselPicker.DrawableItem(R.drawable.ic_abs))
+//        icon_list.add(CarouselPicker.DrawableItem(R.drawable.ic_abs))
+//        icon_list.add(CarouselPicker.DrawableItem(R.drawable.ic_abs))
+//        icon_list.add(CarouselPicker.DrawableItem(R.drawable.ic_abs))
+//        icon_list.add(CarouselPicker.DrawableItem(R.drawable.ic_abs))
+//
+//        val adapter: CarouselPicker.CarouselViewAdapter = CarouselPicker.CarouselViewAdapter(this, icon_list, 0)
+//        carousel!!.adapter = adapter
+
     }
 }
