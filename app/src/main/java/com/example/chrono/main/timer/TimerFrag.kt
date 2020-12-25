0c32374706f8e712555b68858661c0c7990a9bc7
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-12-25 17:04:40 -0500

    timer countdown functionality done (just the count down)
    99.9/100 chance that there won't be any skipped seconds. This is as accurate as I can make the countdown.

--- app/src/main/java/com/example/chrono/main/timer/TimerFrag.kt
+++ app/src/main/java/com/example/chrono/main/timer/TimerFrag.kt
@@ -1,30 +1,67 @@
 package com.example.chrono.main.timer
 
-import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.chrono.R
 import com.example.chrono.databinding.FragmentTimerBinding
+import kotlin.math.roundToInt
 
 
 class TimerFrag : Fragment() {
     var bind: FragmentTimerBinding? = null
 
+    enum class TimerState { RUNNING, STOPPED, PAUSED }
+
+    enum class RunningState { INITIAL, WORK, REST }
+
+    private lateinit var countdown: CountDownTimer
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         bind = DataBindingUtil.inflate(inflater, R.layout.fragment_timer, container, false)
 
-        bind!!.createTimer.setOnClickListener {
-            startActivity(Intent(requireContext(), TimerCreate::class.java))
+//        bind!!.createTimer.setOnClickListener {
+//            startActivity(Intent(requireContext(), TimerCreate::class.java))
+//        }
+
+        countdown = object : CountDownTimer(20000, 1000) {
+            var secondsLeft = 0.toFloat()
+
+            override fun onTick(p0: Long) {
+                if ((p0.toFloat().roundToInt() / (1000).toFloat()) != secondsLeft) {
+                    secondsLeft = (p0.toFloat() / (1000).toFloat()).roundToInt().toFloat()
+                    bind!!.countdown.text = (secondsLeft).toInt().toString()
+                }
+            }
+
+            override fun onFinish() {
+                Toast.makeText(context, "Timer completed.", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        bind!!.startstopbutton.setOnClickListener {
+            countdown.start()
         }
 
         return bind!!.root
+    }
+
+    // Update UI for every tick
+    fun updateUI() {
+
+    }
+
+    // Create a countdown timer based on parameters
+    fun createTimer() {
+
     }
 }
--- app/src/main/java/com/example/chrono/util/PrefUtil.kt
+++ app/src/main/java/com/example/chrono/util/PrefUtil.kt
@@ -7,62 +7,62 @@ import com.example.chrono.main.timer.TimerFrag
 class PrefUtil {
     companion object {
 
-        private const val TIMER_LENGTH_ID = "com.example.chrono.timer_length"
-        private const val TIMER_STATE_ID = "com.example.chrono.timer_state"
-        private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID =
-            "com.example.chrono.previous_timer_length_seconds"
-        private const val SECONDS_REMAINING_ID = "com.example.chrono.seconds_remaining"
-        private const val ALARM_SET_TIME_ID = "com.example.chrono.backgrounded_time"
-
-        fun getTimerLength(context: Context): Int {
-            // placeholder
-            return 1
-        }
-
-        fun getPreviousTimerLengthSeconds(context: Context): Long {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
-        }
-
-        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context) {
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
-            editor.apply()
-        }
-
-        fun getTimerState(context: Context): TimerFrag.TimerState {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
-            return TimerFrag.TimerState.values()[ordinal]
-        }
-
-        fun setTimerState(state: TimerFrag.TimerState, context: Context) {
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            val ordinal = state.ordinal
-            editor.putInt(TIMER_STATE_ID, ordinal)
-            editor.apply()
-        }
-
-        fun getSecondsRemaining(context: Context): Long {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return preferences.getLong(SECONDS_REMAINING_ID, 0)
-        }
-
-        fun setSecondsRemaining(seconds: Long, context: Context) {
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putLong(SECONDS_REMAINING_ID, seconds)
-            editor.apply()
-        }
-
-        fun getAlarmSetTime(context: Context): Long {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return preferences.getLong(ALARM_SET_TIME_ID, 0)
-        }
-
-        fun setAlarmSetTime(time: Long, context: Context) {
-            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putLong(ALARM_SET_TIME_ID, time)
-            editor.apply()
-        }
+//        private const val TIMER_LENGTH_ID = "com.example.chrono.timer_length"
+//        private const val TIMER_STATE_ID = "com.example.chrono.timer_state"
+//        private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID =
+//            "com.example.chrono.previous_timer_length_seconds"
+//        private const val SECONDS_REMAINING_ID = "com.example.chrono.seconds_remaining"
+//        private const val ALARM_SET_TIME_ID = "com.example.chrono.backgrounded_time"
+//
+//        fun getTimerLength(context: Context): Int {
+//            // placeholder
+//            return 1
+//        }
+//
+//        fun getPreviousTimerLengthSeconds(context: Context): Long {
+//            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+//            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
+//        }
+//
+//        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context) {
+//            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+//            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
+//            editor.apply()
+//        }
+//
+//        fun getTimerState(context: Context): TimerFrag.TimerState {
+//            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+//            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
+//            return TimerFrag.TimerState.values()[ordinal]
+//        }
+//
+//        fun setTimerState(state: TimerFrag.TimerState, context: Context) {
+//            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+//            val ordinal = state.ordinal
+//            editor.putInt(TIMER_STATE_ID, ordinal)
+//            editor.apply()
+//        }
+//
+//        fun getSecondsRemaining(context: Context): Long {
+//            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+//            return preferences.getLong(SECONDS_REMAINING_ID, 0)
+//        }
+//
+//        fun setSecondsRemaining(seconds: Long, context: Context) {
+//            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+//            editor.putLong(SECONDS_REMAINING_ID, seconds)
+//            editor.apply()
+//        }
+//
+//        fun getAlarmSetTime(context: Context): Long {
+//            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+//            return preferences.getLong(ALARM_SET_TIME_ID, 0)
+//        }
+//
+//        fun setAlarmSetTime(time: Long, context: Context) {
+//            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+//            editor.putLong(ALARM_SET_TIME_ID, time)
+//            editor.apply()
+//        }
     }
 }
