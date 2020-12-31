b320b9dccabfbf2f842ccc7630a252725e81f41d
Sai Vikranth Desu <vikranthdesu@gmail.com>
2020-12-30 22:29:31 -0500

    updated packages, got rid of deprecated functions

--- app/src/main/java/com/example/chrono/util/BaseActivity.kt
+++ app/src/main/java/com/example/chrono/util/BaseActivity.kt
@@ -1,23 +1,19 @@
 package com.example.chrono.util
 
 import android.content.res.Configuration
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chrono.util.objects.CircuitsObject
 import com.example.chrono.util.objects.PreferenceManager
 
 open class BaseActivity : AppCompatActivity() {
 
-    var circuits: CircuitsObject? = null
+    private var circuits: CircuitsObject? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PreferenceManager.with(this)
 
-        circuits = CircuitsObject()
         circuits = PreferenceManager.get<CircuitsObject>("CIRCUITS")
 
         if (circuits == null) {
@@ -51,15 +47,6 @@ open class BaseActivity : AppCompatActivity() {
             Configuration.UI_MODE_NIGHT_NO -> false
             Configuration.UI_MODE_NIGHT_UNDEFINED -> false
             else -> false
-        }
-    }
-
-    // The function to set the status bar invisible, private since it's only used in this class
-    private fun setStatusBarInvisible() {
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        if (Build.VERSION.SDK_INT >= 21) {
-            window.statusBarColor = Color.TRANSPARENT
         }
     }
 }
