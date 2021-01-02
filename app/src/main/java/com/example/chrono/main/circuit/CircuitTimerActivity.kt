6720c4139121de95b05a571b5980f453a5de7aa9
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-01-02 00:45:04 -0500

    new UI + get ready state

--- app/src/main/java/com/example/chrono/main/circuit/CircuitTimerActivity.kt
+++ app/src/main/java/com/example/chrono/main/circuit/CircuitTimerActivity.kt
@@ -1,11 +1,15 @@
 package com.example.chrono.main.circuit
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.media.AudioManager
+import android.media.ToneGenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.chrono.R
 import com.example.chrono.databinding.ActivityCircuitTimerBinding
@@ -19,11 +23,11 @@ class CircuitTimerActivity : AppCompatActivity() {
     enum class TimerState { INIT, RUNNING, PAUSED }
 
     // Should only be work state and rest state, maybe an in between state? Not an initial state.
-    enum class RunningState { INIT, WORK, REST }
+    enum class RunningState { READY, INIT, WORK, REST, FINAL }
 
     private lateinit var countdown: CountDownTimer
     private var timerState: TimerState = TimerState.INIT
-    private lateinit var runningState: RunningState
+    private var runningState: RunningState = RunningState.INIT
 
     private var secondsLeft: Float = 0.0f
     private var currentSet: Int = 0
@@ -32,7 +36,8 @@ class CircuitTimerActivity : AppCompatActivity() {
     private var timeRest: Int = 0
     private var timeWork: Int = 0
 
-//    private lateinit var audioPlayer: AsyncPlayer
+    //    private lateinit var audioPlayer: AsyncPlayer
+    private var tone: ToneGenerator = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,28 +50,41 @@ class CircuitTimerActivity : AppCompatActivity() {
         )
 
         // Initialize stuff perhaps
+        updateButtonUI()
+        updateRestUI()
 
-        bind!!.startbutton.setOnClickListener {
-            workout()
+        bind!!.startButton.setOnClickListener {
             timerState = TimerState.RUNNING
-            updateButtonUI()
+            getReady()
+            tone.startTone(ToneGenerator.TONE_DTMF_C, 550)
         }
 
-        bind!!.pausebutton.setOnClickListener {
+        bind!!.pauseButton.setOnClickListener {
             countdown.cancel()
             timerState = TimerState.PAUSED
             updateButtonUI()
         }
 
-        bind!!.resumebutton.setOnClickListener {
+        bind!!.resumeButton.setOnClickListener {
             startTimer(secondsLeft.toInt(), true)
             timerState = TimerState.RUNNING
+            updateRestUI()
             updateButtonUI()
+        }
+
+        bind!!.stopButton.setOnClickListener {
+            countdown.cancel()
+            timerState = TimerState.INIT
+            runningState = RunningState.INIT
+            updateButtonUI()
+            updateRestUI()
         }
 
         bind!!.closeButton.setOnClickListener {
             // Make sure that the timer is shut down
-            countdown.cancel()
+            if (timerState != TimerState.INIT) {
+                countdown.cancel()
+            }
             setResult(Activity.RESULT_CANCELED)
             finish()
         }
@@ -88,7 +106,7 @@ class CircuitTimerActivity : AppCompatActivity() {
             override fun onFinish() {
                 if (currentSet != 0) {
                     when (runningState) {
-                        RunningState.INIT -> {
+                        RunningState.READY -> {
                             workout()
                         }
                         RunningState.WORK -> {
@@ -97,6 +115,7 @@ class CircuitTimerActivity : AppCompatActivity() {
                         RunningState.REST -> {
                             workout()
                         }
+                        else -> isDone()
                     }
                 } else {
                     isDone()
@@ -105,11 +124,33 @@ class CircuitTimerActivity : AppCompatActivity() {
         }.start()
     }
 
+    private fun getReady() {
+        runningState = RunningState.READY
+        bind!!.initButtonLayout.visibility = View.GONE
+        updateRestUI()
+        startTimer(5, false)
+    }
+
     private fun isDone() {
         Toast.makeText(this, "It's done", Toast.LENGTH_SHORT).show()
         timerState = TimerState.INIT
         runningState = RunningState.INIT
         updateButtonUI()
+        updateRestUI()
+    }
+
+    private fun workout() {
+        runningState = RunningState.WORK
+        updateRestUI()
+        updateButtonUI()
+        startTimer(timeWork, false)
+    }
+
+    private fun rest() {
+        runningState = RunningState.REST
+        updateRestUI()
+        startTimer(timeRest, false)
+        currentSet -= 1
     }
 
     // Update UI for every tick, possibly need to do more in the future
@@ -120,20 +161,81 @@ class CircuitTimerActivity : AppCompatActivity() {
     private fun updateButtonUI() {
         when (timerState) {
             TimerState.INIT -> {
-                bind!!.initButtonLay.visibility = View.VISIBLE
-                bind!!.runButtonLay.visibility = View.GONE
-                bind!!.pauseButtonLay.visibility = View.GONE
+                bind!!.initButtonLayout.visibility = View.VISIBLE
+                bind!!.runButtonLayout.visibility = View.GONE
+                bind!!.pauseButtonLayout.visibility = View.GONE
                 bind!!.countdown.text = "0"
             }
             TimerState.RUNNING -> {
-                bind!!.initButtonLay.visibility = View.GONE
-                bind!!.runButtonLay.visibility = View.VISIBLE
-                bind!!.pauseButtonLay.visibility = View.GONE
+                bind!!.initButtonLayout.visibility = View.GONE
+                bind!!.runButtonLayout.visibility = View.VISIBLE
+                bind!!.pauseButtonLayout.visibility = View.GONE
             }
             TimerState.PAUSED -> {
-                bind!!.initButtonLay.visibility = View.GONE
-                bind!!.runButtonLay.visibility = View.GONE
-                bind!!.pauseButtonLay.visibility = View.VISIBLE
+                bind!!.initButtonLayout.visibility = View.GONE
+                bind!!.runButtonLayout.visibility = View.GONE
+                bind!!.pauseButtonLayout.visibility = View.VISIBLE
+
+                // Just for paused we put non-button UI stuff
+                bind!!.mainLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.countdown.setTextColor(ContextCompat.getColor(this, R.color.black))
+                bind!!.currentSet.setTextColor(ContextCompat.getColor(this, R.color.black))
+                bind!!.currentState.setTextColor(ContextCompat.getColor(this, R.color.black))
+                bind!!.closeButton.setImageResource(R.drawable.ic_close_grey)
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateRestUI() {
+        when (runningState) {
+            RunningState.READY -> {
+                bind!!.currentState.text = getString(R.string.get_ready)
+            }
+            RunningState.INIT -> {
+                bind!!.currentSet.text = getString(R.string.empty)
+                bind!!.currentState.text = getString(R.string.work)
+                bind!!.mainLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.countdown.setTextColor(ContextCompat.getColor(this, R.color.black))
+                bind!!.currentSet.setTextColor(ContextCompat.getColor(this, R.color.black))
+                bind!!.currentState.setTextColor(ContextCompat.getColor(this, R.color.black))
+                bind!!.closeButton.setImageResource(R.drawable.ic_close_grey)
+            }
+            RunningState.WORK -> {
+                bind!!.currentState.text = getString(R.string.workout)
+                bind!!.currentSet.text = "Set " + (sets - currentSet.toString().toInt() + 1)
+
+                bind!!.mainLayout.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.beautiful_blue
+                    )
+                )
+                bind!!.countdown.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.currentSet.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.currentState.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.closeButton.setImageResource(R.drawable.ic_close_white)
+            }
+            RunningState.REST -> {
+                bind!!.currentState.text = getString(R.string.rest)
+
+                bind!!.mainLayout.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.rest_yellow
+                    )
+                )
+                bind!!.countdown.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.currentSet.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.currentState.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.closeButton.setImageResource(R.drawable.ic_close_white)
+            }
+            RunningState.FINAL -> {
+                bind!!.mainLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.stop_red))
+                bind!!.countdown.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.currentSet.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.currentState.setTextColor(ContextCompat.getColor(this, R.color.white))
+                bind!!.closeButton.setImageResource(R.drawable.ic_close_white)
             }
         }
     }
@@ -143,22 +245,5 @@ class CircuitTimerActivity : AppCompatActivity() {
         currentSet = sets
         timeRest = circuit.rest!!
         timeWork = circuit.work!!
-
-        bind!!.name.text = circuit.name
-    }
-
-    private fun workout() {
-        runningState = RunningState.WORK
-        bind!!.state.text = "Workout"
-        bind!!.set.text = "Set Number " + currentSet.toString()
-        startTimer(timeWork, false)
-    }
-
-    private fun rest() {
-        runningState = RunningState.REST
-        bind!!.state.text = "Rest"
-        bind!!.set.text = "Set Number " + currentSet.toString()
-        startTimer(timeRest, false)
-        currentSet -= 1
     }
 }
