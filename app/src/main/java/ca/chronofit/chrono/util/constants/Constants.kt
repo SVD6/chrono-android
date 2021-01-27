5523d11b329d4769c1a7c2a269127aafeb95083c
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-01-27 01:12:25 -0500

    added constants for fragment tags

--- app/src/main/java/ca/chronofit/chrono/util/constants/Constants.kt
+++ app/src/main/java/ca/chronofit/chrono/util/constants/Constants.kt
@@ -1,6 +1,10 @@
 package ca.chronofit.chrono.util.constants
 
 object Constants {
+    // Fragment TAGS
+    const val CIRCUIT_FRAG: String = "CircuitFrag"
+    const val STOPWATCH_FRAG: String = "StopwatchFrag"
+    const val SETTINGS_FRAG: String = "SettingsFrag"
 
     // Shared Preference Values
     const val CIRCUITS: String = "CIRCUITS"
