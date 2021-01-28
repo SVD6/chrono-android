b3458420bc26a0f9bf95d17a2fd1b7784cdc03d2
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-01-28 00:50:00 -0500

    cleaned up

--- app/src/main/java/ca/chronofit/chrono/circuit/CircuitTimerActivity.kt
+++ app/src/main/java/ca/chronofit/chrono/circuit/CircuitTimerActivity.kt
@@ -10,7 +10,6 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
-import android.view.Window
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
