228e13f24fe63b2467cd30ac0d82dbec9b115315
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-05-10 23:31:52 -0400

    Add the stopwatch "gif" into onbaording

--- app/src/main/java/ca/chronofit/chrono/onboarding/OnBoardSwatchFrag.kt
+++ app/src/main/java/ca/chronofit/chrono/onboarding/OnBoardSwatchFrag.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import ca.chronofit.chrono.R
 import ca.chronofit.chrono.databinding.FragmentOnboardSwatchBinding
+import com.bumptech.glide.Glide
 
 class OnBoardSwatchFrag : Fragment() {
     private lateinit var bind: FragmentOnboardSwatchBinding
@@ -19,6 +20,9 @@ class OnBoardSwatchFrag : Fragment() {
         // Inflate the layout for this fragment
         bind =
             DataBindingUtil.inflate(inflater, R.layout.fragment_onboard_swatch, container, false)
+
+        Glide.with(requireContext()).asGif().load(R.raw.onboard_stopwatch).into(bind.imageGif)
+
         return bind.root
     }
 }
