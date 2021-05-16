8440f0ef2162b4f60f48fd20ff3e0c45a80e83ab
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-05-16 01:26:44 -0400

    Fix small things in settings fragment

--- app/src/main/java/ca/chronofit/chrono/settings/SettingsFrag.kt
+++ app/src/main/java/ca/chronofit/chrono/settings/SettingsFrag.kt
@@ -181,14 +181,14 @@ class SettingsFrag : Fragment() {
                 startActivity(
                     Intent(
                         Intent.ACTION_VIEW,
-                        Uri.parse(getString(R.string.app_rating_primary_link) + "$packageName")
+                        Uri.parse(getString(R.string.app_rating_primary_link) + packageName)
                     )
                 )
             } catch (e: ActivityNotFoundException) {
                 startActivity(
                     Intent(
                         Intent.ACTION_VIEW,
-                        Uri.parse(getString(R.string.app_rating_secondary_link) + "$packageName")
+                        Uri.parse(getString(R.string.app_rating_secondary_link) + packageName)
                     )
                 )
             }
@@ -212,7 +212,7 @@ class SettingsFrag : Fragment() {
         // Set Version Number
         try {
             bind.versionNumber.text =
-                "Test Version " + requireContext().packageManager.getPackageInfo(
+                "Version " + requireContext().packageManager.getPackageInfo(
                     requireContext().packageName,
                     0
                 ).versionName
